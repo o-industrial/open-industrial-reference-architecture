8a779b83274c6e44781cb43633b4d1ea5406e372
Skeletor11 <kevinbowersdeveloper@gmail.com>
2025-05-20 15:00:19 -0600

    more fixes

--- src/eac/EaCWarmQueryDetails.ts
+++ src/eac/EaCWarmQueryDetails.ts
@@ -2,22 +2,14 @@ import { EaCVertexDetails, EaCVertexDetailsSchema, z } from "./.deps.ts";
 
 export type EaCWarmQueryDetails = {
   Version: number;
-
   Query: string;
 } & EaCVertexDetails;
 
-export const EaCWarmQueryDetailsSchema: z.ZodObject<
-  {
-    Description: z.ZodOptional<z.ZodString>;
-    Name: z.ZodOptional<z.ZodString>;
-  },
-  'strip',
-  z.ZodTypeAny,
-  EaCVertexDetails,
-  EaCVertexDetails
-> = EaCVertexDetailsSchema.describe(
-  'Schema for warm query-level metadata and attributes.',
-);
+export const EaCWarmQueryDetailsSchema: z.ZodType<EaCWarmQueryDetails> =
+  EaCVertexDetailsSchema.extend({
+    Version: z.number(),
+    Query: z.string(),
+  }).describe("Schema for warm query-level metadata and attributes.");
 
 export function isEaCWarmQueryDetails(
   details: unknown,
@@ -25,7 +17,8 @@ export function isEaCWarmQueryDetails(
   return EaCWarmQueryDetailsSchema.safeParse(details).success;
 }
 
-export function parseEaCWarmQueryDetails(details: unknown): EaCWarmQueryDetails {
+export function parseEaCWarmQueryDetails(
+  details: unknown,
+): EaCWarmQueryDetails {
   return EaCWarmQueryDetailsSchema.parse(details);
 }
-
