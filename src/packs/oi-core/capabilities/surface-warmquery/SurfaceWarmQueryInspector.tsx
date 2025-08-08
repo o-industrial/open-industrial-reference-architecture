0d11b00a2706158f7748daf9024f9fed763a581c
Skeletor11 <kevinbowersdeveloper@gmail.com>
2025-08-08 11:10:52 -0600

    methods

--- src/packs/oi-core/capabilities/surface-warmquery/SurfaceWarmQueryInspector.tsx
+++ src/packs/oi-core/capabilities/surface-warmquery/SurfaceWarmQueryInspector.tsx
@@ -83,8 +83,8 @@ export function SurfaceWarmQueryInspector({
           queryText={details.Query ?? ''}
           queryApiPath={details.ApiPath ?? ''}
           onClose={handleCloseModal}
-          onRun={handleRunQuery}
-          onSave={handleSaveQuery}
+          onRun={() => handleRunQuery(details.Query ?? '')}
+          onSave={() => handleSaveQuery(details.Name ?? '', details.ApiPath ?? '', details.Description ?? '', details.Query ?? '')}
         />
       )}
     </>
